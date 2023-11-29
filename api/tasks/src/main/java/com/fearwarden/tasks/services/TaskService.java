fa45197afc4f44d9bbe756556b1f077c19cbf4a6
Boris Antonijev <81489899+fearwarden@users.noreply.github.com>
2023-11-29 23:43:42 +0100

    create task service

--- api/tasks/src/main/java/com/fearwarden/tasks/services/TaskService.java
+++ api/tasks/src/main/java/com/fearwarden/tasks/services/TaskService.java
@@ -1,4 +1,7 @@
 package com.fearwarden.tasks.services;
 
+import com.fearwarden.basemodule.models.UserEntity;
+
 public interface TaskService {
+    void create(String description, Integer categoryId, Integer priorityId, Integer statusId, UserEntity user);
 }
--- api/tasks/src/main/java/com/fearwarden/tasks/services/implementations/TaskServiceImpl.java
+++ api/tasks/src/main/java/com/fearwarden/tasks/services/implementations/TaskServiceImpl.java
@@ -1,8 +1,41 @@
 package com.fearwarden.tasks.services.implementations;
 
+import com.fearwarden.basemodule.models.CategoryEntity;
+import com.fearwarden.basemodule.models.UserEntity;
+import com.fearwarden.basemodule.repositories.CategoryRepository;
+import com.fearwarden.tasks.exceptions.throwables.CategoryNotFoundException;
+import com.fearwarden.tasks.exceptions.throwables.PriorityNotFoundException;
+import com.fearwarden.tasks.exceptions.throwables.StatusNotFoundException;
+import com.fearwarden.tasks.models.PriorityEntity;
+import com.fearwarden.tasks.models.StatusEntity;
+import com.fearwarden.tasks.models.TaskEntity;
+import com.fearwarden.tasks.repositories.PriorityRepository;
+import com.fearwarden.tasks.repositories.StatusRepository;
+import com.fearwarden.tasks.repositories.TaskRepository;
 import com.fearwarden.tasks.services.TaskService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class TaskServiceImpl implements TaskService {
+    private final TaskRepository taskRepository;
+    private final PriorityRepository priorityRepository;
+    private final StatusRepository statusRepository;
+    private final CategoryRepository categoryRepository;
+
+    @Override
+    public void create(String description, Integer categoryId, Integer priorityId, Integer statusId, UserEntity user) {
+        CategoryEntity category = this.categoryRepository.findById(categoryId).orElseThrow(CategoryNotFoundException::new);
+        PriorityEntity priority = this.priorityRepository.findById(priorityId).orElseThrow(PriorityNotFoundException::new);
+        StatusEntity status = this.statusRepository.findById(statusId).orElseThrow(StatusNotFoundException::new);
+
+        TaskEntity task = new TaskEntity();
+        task.setDescription(description);
+        task.setCategoryEntity(category);
+        task.setPriorityEntity(priority);
+        task.setStatusEntity(status);
+        task.setUserEntity(user);
+        this.taskRepository.save(task);
+    }
 }
