f2a4d06ccd18a65b81fdfe784701b7c78af5c8c8
Boris Antonijev <81489899+fearwarden@users.noreply.github.com>
2023-12-04 18:44:06 +0100

    Update TaskServiceImpl.java

--- api/tasks/src/main/java/com/fearwarden/tasks/services/implementations/TaskServiceImpl.java
+++ api/tasks/src/main/java/com/fearwarden/tasks/services/implementations/TaskServiceImpl.java
@@ -19,7 +19,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
