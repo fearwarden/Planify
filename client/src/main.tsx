35167cc3780a85db8a78333521374b195ef13e05
Boris Antonijev <81489899+fearwarden@users.noreply.github.com>
2024-01-17 16:05:12 +0100

    setup routing

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,12 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App.tsx";
+import { RouterProvider } from "react-router-dom";
+import { Provider } from "react-redux";
+import "@/index.css";
+import Router from "@/routes/Router";
+import { store } from "@/store/store";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <RouterProvider router={Router.router} />
+  </Provider>
 );
--- client/src/routes/Router.tsx
+++ client/src/routes/Router.tsx
@@ -1,5 +1,17 @@
-import {createBrowserRouter} from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
+import { HOME, LOGIN } from "@/constants/constants";
+import Login from "@/components/Login/Login";
+import Home from "@/pages/Home/Home";
 
 export default class Router {
-    public static readonly router = createBrowserRouter([])
+  public static readonly router = createBrowserRouter([
+    {
+      path: HOME,
+      element: <Home />,
+    },
+    {
+      path: LOGIN,
+      element: <Login />,
+    },
+  ]);
 }
