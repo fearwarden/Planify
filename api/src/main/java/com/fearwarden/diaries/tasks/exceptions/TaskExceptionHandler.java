2d86d16dba7bf9ca2b5d9cac98fa68144b7f254c
Boris Antonijev <81489899+fearwarden@users.noreply.github.com>
2024-01-19 14:52:36 +0100

    Update TaskExceptionHandler.java

--- api/src/main/java/com/fearwarden/diaries/tasks/exceptions/TaskExceptionHandler.java
+++ api/src/main/java/com/fearwarden/diaries/tasks/exceptions/TaskExceptionHandler.java
@@ -3,6 +3,8 @@ package com.fearwarden.diaries.tasks.exceptions;
 import com.fearwarden.diaries.tasks.exceptions.throwables.CategoryNotFoundException;
 import com.fearwarden.diaries.tasks.exceptions.throwables.PriorityNotFoundException;
 import com.fearwarden.diaries.tasks.exceptions.throwables.TaskNotFoundException;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
+@Order(Ordered.HIGHEST_PRECEDENCE)
 public class TaskExceptionHandler {
 
     @ResponseBody
