92c83b85a410a5ee5ed3bfdbb13d8141b962b979
Boris Antonijev <81489899+fearwarden@users.noreply.github.com>
2024-01-23 14:02:25 +0100

    [frontend] feat: add tanstack query devtools

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,6 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "@/index.css";
 import Router from "@/routes/Router";
 import { store } from "@/store/store";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient();
 
@@ -12,6 +13,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <QueryClientProvider client={queryClient}>
     <Provider store={store}>
       <RouterProvider router={Router.router} />
+      <ReactQueryDevtools />
     </Provider>
   </QueryClientProvider>
 );
