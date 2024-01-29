1f857f765420f7cd490bbc0f8850cd8a56f79e4e
Boris Antonijev <81489899+fearwarden@users.noreply.github.com>
2024-01-29 20:26:34 +0100

    [frontend] style: remove task test page

--- client/src/routes/Router.tsx
+++ client/src/routes/Router.tsx
@@ -2,7 +2,6 @@ import { createBrowserRouter } from "react-router-dom";
 import { HOME, LOGIN } from "@/constants/constants";
 import Login from "@/components/Login/Login";
 import Home from "@/pages/Home/Home";
-import Task from "@/pages/Home/components/Task";
 
 export default class Router {
   public static readonly router = createBrowserRouter([
@@ -13,10 +12,6 @@ export default class Router {
     {
       path: LOGIN,
       element: <Login />,
-    },
-    {
-      path: "/task",
-      element: <Task />,
     },
   ]);
 }
