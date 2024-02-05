54ac94f9854f83afdffc7b1fa59854c73328c40d
Srdjan196 <srdjanrogic17@gmail.com>
2024-02-05 16:23:49 +0100

    Added SearchBar

--- client/src/components/Navigations/NavBar.tsx
+++ client/src/components/Navigations/NavBar.tsx
@@ -2,14 +2,30 @@ function NavBar() {
   return (
     <div className="h-[140px] w-full overflow-hidden pb-2.5">
       <div className="mt-2.5 flex h-full w-full flex-col rounded-[28px] bg-black bg-opacity-60 bg-blend-normal">
-        <div className="h-[140px] flex items-center justify-between">
-          <div className="ml-4 flex items-center">
-            <input
-              type="text"
-              placeholder="Search..."
-              className="px-4 py-2 rounded-md bg-gray-200 focus:outline-none text-black"
-            />
-          </div>
+        <div className="h-[140px] flex items-center justify-between px-4">
+          <form className="max-w-sm px-4">
+            <div className="relative">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="absolute top-0 bottom-0 w-6 h-6 my-auto text-gray-400 left-3"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                />
+              </svg>
+              <input
+                type="text"
+                placeholder="Search"
+                className="w-full py-3 pl-12 pr-4 text-gray-500 border rounded-[28px] outline-none bg-gray-50 focus:bg-white focus:border-indigo-600"
+              />
+            </div>
+          </form>
           <div className="flex-grow flex justify-center">
             <div className="flex space-x-6 text-white">
               <div className="cursor-pointer">Tasks</div>
