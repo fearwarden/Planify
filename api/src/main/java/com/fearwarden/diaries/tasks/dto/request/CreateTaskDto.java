a3c98dc1b30e1f4307e0f628f74d4dfe8f3add4b
Boris Antonijev <81489899+fearwarden@users.noreply.github.com>
2024-02-29 15:21:07 +0100

    [backend] fix: fix update and create dto

--- api/src/main/java/com/fearwarden/diaries/tasks/dto/request/CreateTaskDto.java
+++ api/src/main/java/com/fearwarden/diaries/tasks/dto/request/CreateTaskDto.java
@@ -1,9 +1,6 @@
 package com.fearwarden.diaries.tasks.dto.request;
 
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.*;
 import lombok.Data;
 
 import java.time.LocalDateTime;
@@ -12,17 +9,18 @@ import java.time.LocalDateTime;
 public class CreateTaskDto {
     @NotNull(message = "Description is required.")
     @NotBlank(message = "Description should not be blank.")
+    @Size(max = 255, message = "Description must be less than 255 characters.")
     private String description;
     @NotNull(message = "Due is required.")
     @NotBlank(message = "Due should not be blank.")
     private String due;
     @NotNull(message = "Category ID is required.")
-    @Min(0)
+    @Min(1)
     private Integer categoryId;
     @NotNull(message = "Priority ID is required.")
-    @Min(0)
+    @Min(1)
     private Integer priorityId;
     @NotNull(message = "Status ID is required.")
-    @Min(0)
+    @Min(1)
     private Integer statusId;
 }
--- api/src/main/java/com/fearwarden/diaries/tasks/dto/request/UpdateTaskDto.java
+++ api/src/main/java/com/fearwarden/diaries/tasks/dto/request/UpdateTaskDto.java
@@ -1,27 +1,24 @@
 package com.fearwarden.diaries.tasks.dto.request;
 
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.*;
 import lombok.Data;
 
 @Data
 public class UpdateTaskDto {
     @NotNull(message = "Description is required.")
     @NotBlank(message = "Description should not be blank.")
-    @Max(value = 255, message = "Description should be less then 255 characters.")
+    @Size(max = 255, message = "Description must be less than 255 characters.")
     private String description;
     @NotNull(message = "Due is required.")
     @NotBlank(message = "Due should not be blank.")
     private String due;
     @NotNull(message = "Category ID is required.")
-    @Min(0)
+    @Min(1)
     private Integer categoryId;
     @NotNull(message = "Priority ID is required.")
-    @Min(0)
+    @Min(1)
     private Integer priorityId;
     @NotNull(message = "Status ID is required.")
-    @Min(0)
+    @Min(1)
     private Integer statusId;
 }
