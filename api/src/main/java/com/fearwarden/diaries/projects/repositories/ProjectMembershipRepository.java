9d811006e4a33f4f8f2179eb7d1a3f0cd610e6d9
Boris Antonijev <81489899+fearwarden@users.noreply.github.com>
2024-08-26 21:22:38 +0200

    [backend] feat: custom repository functions for fetching project based on membership and user

--- api/src/main/java/com/fearwarden/diaries/projects/repositories/ProjectMembershipRepository.java
+++ api/src/main/java/com/fearwarden/diaries/projects/repositories/ProjectMembershipRepository.java
@@ -1,9 +1,12 @@
 package com.fearwarden.diaries.projects.repositories;
 
 import com.fearwarden.diaries.projects.models.ProjectMembershipEntity;
+import com.fearwarden.diaries.users.models.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Set;
 import java.util.UUID;
 
 public interface ProjectMembershipRepository extends JpaRepository<ProjectMembershipEntity, UUID> {
+    Set<ProjectMembershipEntity> findAllByUserEntity(UserEntity user);
 }
--- api/src/main/java/com/fearwarden/diaries/projects/repositories/ProjectRepository.java
+++ api/src/main/java/com/fearwarden/diaries/projects/repositories/ProjectRepository.java
@@ -1,9 +1,14 @@
 package com.fearwarden.diaries.projects.repositories;
 
 import com.fearwarden.diaries.projects.models.ProjectEntity;
+import com.fearwarden.diaries.projects.models.ProjectMembershipEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
 import java.util.UUID;
 
 public interface ProjectRepository extends JpaRepository<ProjectEntity, UUID> {
+    ProjectEntity findAllById(UUID id);
 }
