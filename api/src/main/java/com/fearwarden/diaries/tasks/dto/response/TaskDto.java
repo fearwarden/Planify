b7b93c3cd357771eafebb33585510154158755e1
Boris Antonijev <81489899+fearwarden@users.noreply.github.com>
2024-08-27 15:51:55 +0200

    [backend] returning entities in task dto for metadata

--- api/src/main/java/com/fearwarden/diaries/tasks/dto/response/TaskDto.java
+++ api/src/main/java/com/fearwarden/diaries/tasks/dto/response/TaskDto.java
@@ -1,5 +1,8 @@
 package com.fearwarden.diaries.tasks.dto.response;
 
+import com.fearwarden.diaries.metadata.models.CategoryEntity;
+import com.fearwarden.diaries.metadata.models.PriorityEntity;
+import com.fearwarden.diaries.metadata.models.StatusEntity;
 import com.fearwarden.diaries.tasks.models.TaskEntity;
 import com.fearwarden.diaries.tasks.tools.HelperFunctions;
 import lombok.Data;
@@ -14,9 +17,9 @@ public class TaskDto {
     private String createdAt;
     private String updatedAt;
     private String userId;
-    private String category;
-    private String priority;
-    private String status;
+    private CategoryEntity category;
+    private PriorityEntity priority;
+    private StatusEntity status;
 
     public TaskDto(TaskEntity task) {
         this.id = task.getId().toString();
@@ -25,8 +28,8 @@ public class TaskDto {
         this.createdAt = HelperFunctions.convertDateToString(task.getCreatedAt());
         this.updatedAt = HelperFunctions.convertDateToString(task.getUpdatedAt());
         this.userId = task.getUserEntity().getId().toString();
-        this.category = task.getCategoryEntity().getName();
-        this.priority = task.getPriorityEntity().getLevel();
-        this.status = task.getStatusEntity().getProgress();
+        this.category = task.getCategoryEntity();
+        this.priority = task.getPriorityEntity();
+        this.status = task.getStatusEntity();
     }
 }
