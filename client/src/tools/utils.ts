2f882f9b311c425c61a5356fb0cc87e9f56568c6
Boris Antonijev <81489899+fearwarden@users.noreply.github.com>
2024-08-27 15:54:32 +0200

    [frontend] converting current date to local date time

--- client/src/tools/utils.ts
+++ client/src/tools/utils.ts
@@ -1,3 +1,5 @@
+import { parse, format } from 'date-fns';
+
 export function convertToTimestamp(date: string, time: string) {
   return date + "T" + time;
 }
@@ -5,4 +7,9 @@ export function convertToTimestamp(date: string, time: string) {
 export function formatDate(dateString: string) {
   const date = new Date(dateString);
   return `${date.toLocaleDateString("en-GB")} ${date.toLocaleTimeString()}`;
+}
+
+export function combineDateWithT(date: string): string {
+  const parsedDate = parse(date, 'MMMM d. yyyy, HH:mm', new Date());
+  return format(parsedDate, "yyyy-MM-dd'T'HH:mm:ss.SSS");
 }
