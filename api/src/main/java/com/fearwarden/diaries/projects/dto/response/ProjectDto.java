05c665e5d4aa45bfa1016199cedf7adbbb57c8ab
fearwarden <antonijevboris@gmail.com>
2024-09-07 23:55:27 +0200

    [backend] in project dto date fields change type to LocalDateTime

--- api/src/main/java/com/fearwarden/diaries/projects/dto/response/ProjectDto.java
+++ api/src/main/java/com/fearwarden/diaries/projects/dto/response/ProjectDto.java
@@ -3,14 +3,15 @@ package com.fearwarden.diaries.projects.dto.response;
 import com.fearwarden.diaries.projects.enums.ProjectStatus;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 /**
  * DTO for {@link com.fearwarden.diaries.projects.models.ProjectEntity}
  */
-public record ProjectDto(UUID id, String name, String createdAt,
-                         String updatedAt, String iconPath, ProjectStatus status, String url) implements Serializable {
-    public ProjectDto withIconPathAndDate(String iconPath, String createdAt, String updatedAt) {
+public record ProjectDto(UUID id, String name, LocalDateTime createdAt,
+                         LocalDateTime updatedAt, String iconPath, ProjectStatus status, String url) implements Serializable {
+    public ProjectDto withIconPath(String iconPath) {
         return new ProjectDto(id, name, createdAt, updatedAt, iconPath, status, url);
     }
 }
